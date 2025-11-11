a00208bef90effe1449cc208241067628325088b
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-11 10:39:56 +0100

    Genero 10 fechas entre ese rango de fechas

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import numpy as np
 import pandas as pd
-import prefect
+from prefect import flow, task
+
+# devuelve un rango de fechas diarias entre dos fechas dadas
+dias = pd.date_range('2021-10-01','2021-10-10', freq='d')
+
+# genera 10 valores aleatorios entre 10 y 200
+#valor_1_cierre = np.random.uniform(low=10, high=200, size=10)
+
+print(dias)
+#print(valor_1_cierre)
