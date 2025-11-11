9d2644212cb022c70867727012a05e0ca808ccc8
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-11 10:40:50 +0100

    Genera 10 numeros aleatorios entre 10 y 200

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from prefect import flow, task
 dias = pd.date_range('2021-10-01','2021-10-10', freq='d')
 
 # genera 10 valores aleatorios entre 10 y 200
-#valor_1_cierre = np.random.uniform(low=10, high=200, size=10)
+valor_1_cierre = np.random.uniform(low=10, high=200, size=10)
 
 print(dias)
-#print(valor_1_cierre)
+print(valor_1_cierre)
